1337efc9eee4efb525cdd17802b82eb965730a14
jeongyoonmin <rapaella1109@gmail.com>
2022-03-24 13:43:46 +0900

    함수값 적용

--- source/220324 롯데월드.cpp
+++ source/220324 롯데월드.cpp
@@ -1,10 +1,6 @@
 #include <stdio.h>
 #include <time.h>
-
-int main(){
-	
-	
-	int choose,ticket,FrontNumber,BackNumber,orderCount,discount,prise,sum,final,next,all,totalTicketPrice,counter,ticketTimeCase,ageCase,again,todaytime;
+int choose,ticket,FrontNumber,BackNumber,orderCount,discount,prise,sum,final,next,all,totalTicketPrice,counter,ticketTimeCase,ageCase,again,todaytime;
 	const int  ALL_AFTER_ADULT_PRICE = 48000, ALL_AFTER_TEEN_PRICE = 42000,ALL_AFTER_CHILD_PRICE = 36000;//종합 4after가격 
 	const int  PARK_AFTER_ADULT_PRICE = 45000,  PARK_AFTER_TEEN_PRICE = 40000, PARK_AFTER_CHILD_PRICE = 35000;//파크 4after가격
 	const int  BABY_PRICE =15000, ALL_DAY_ADULT_PRICE = 59000, ALL_DAY_TEEN_PRICE = 52000, ALL_DAY_CHILD_PRICE = 47000;// 종합 1day가격
@@ -12,13 +8,9 @@ int main(){
 	const int  OLDMAN_AGE=65, TEEN_AGE_MAX=18, TEEN_KID_AGE=13, KID_BABY_AGE=3, BABY_AGE_MIN=1;//나이 최댓값 
 	int numberYear,birth,age,oldage,newage,birthMonth,birthDay,todayDate; 
 	int ticketTimeCaseArr[10]={0}, ageArr[10]={0}, ticketPriceArr[10]={0}, discountArr[10]={0}, orderCountArr[10]={0};
-//권종, 연령구분, 수량, 가격, 우대사항
-
-while(1){
-	int totalTicketPrice=0, counter=0;
-	int icketTimeCaseArr[10]={0}, ageArr[10]={0}, ticketPriceArr[10]={0}, discountArr[10]={0}, orderCountArr[10]={0};
-	do{//c변수 초기화
-	 	choose=0, ticket=0, discount=0, orderCount=0, FrontNumber=0, BackNumber=0;
+	
+	
+	void selectTicket(){//권종 선택 
 		do{
 		printf("선택해주세요.\n1. 종합이용권(롯데월드+민속박물관)  \n2. 파크이용권\n");
 		scanf("%d",&choose);
@@ -28,7 +20,8 @@ while(1){
 		printf("권종을 선택하세요.\n1. 1DAY \n2. AFTER4(오후 4시 이후 입장)\n");
 		scanf("%d",&ticket);
 		}while(!(ticket ==1 || ticket ==2));
-	
+		}
+	void personNumber(){//주민번호 앞자리 6글자 +뒷자리 1글자 
 		do{
 		printf("주민번호 앞자리 6자글자와 뒷자리 1자리를 입력하세요.\n 앞자리(6글자)\n");
 		scanf("%d",&FrontNumber);
@@ -38,25 +31,23 @@ while(1){
 		printf("뒷자리(1글자)\n");
 		scanf("%d",&BackNumber);
 		}while(!(BackNumber==1 || BackNumber==2 || BackNumber==3||BackNumber==4));
-		
+		}
+	void countTicket(){	//주문갯수 
 		do{
 		printf("몇개를 주문하시겠습니가?(최대 10개)\n");
 		scanf("%d",&orderCount);
 		}while(!(orderCount>=1 && orderCount <11));
-	
+		}	
+	void discountMoney(){	//우대사항
 		do{
 		printf("우대사항을 선택하세요.\n1. 없음(나이 우대는 자동처리)\n2. 장애인\n3. 국가유공자\n4. 다둥이 행복\n5. 임산부\n6. 휴가장병 \n");
 		scanf("%d",&discount);
 		}while(!(discount==1 ||discount==2 ||discount==3 ||discount==4 ||discount==5 ||discount==6));
-	
-	
-	
-	
+	}	
+	void monthAge(){ //만나이 계산 
 			struct tm*t;
 			time_t base = time(NULL);
 			t = localtime(&base);
-			
-			
 			numberYear =FrontNumber/10000; //생년 출력
 			birthMonth= (FrontNumber-(numberYear * 10000))*100/10000; // 생월 출력
 			birthDay= FrontNumber-(numberYear * 10000)-(birthMonth * 100); //생일 출력 
@@ -85,36 +76,13 @@ while(1){
 					age= ((t->tm_year + 1900) - newage)-1;
 				}
 			}//age는 만나이
-			 
-			//나이 케이스 정립
-			if ( age <KID_BABY_AGE && age >=BABY_AGE_MIN) { 
-				ageCase=4; // 베이비 
-			} else if ((age >= KID_BABY_AGE && age <TEEN_KID_AGE)) {
-				ageCase = 3; // 어린이 
-			} else if (age >= TEEN_KID_AGE && age <= TEEN_AGE_MAX) {
-				ageCase = 2; //청소년 
-			} else if ( age >= OLDMAN_AGE ) {
-				ageCase = 5; //노 인  
-			} else {
-				ageCase = 1; // 어른 
-			}
-	
-			
-			//종합/파크&& 주간야간 선택 
-			if (choose==1 && ticket==1) {
-				ticketTimeCase = 1; //종합 + 1Day 
-			} else if (choose==1 && ticket==2) { 
-				ticketTimeCase =2; //종합 + 4시후 
-			} else if (choose == 2 && ticket == 1) {
-				ticketTimeCase = 3; //파크 + 1Day 
-			} else {
-				ticketTimeCase = 4; // 파크 + 4시후 
-			} 
+	}
+	void chooseAgePrice(){ //티켓별 나이대 별 가격 
 			switch(choose){
 				case 1 :
 					switch(ticket){
 					case 1:
-	
+
 					if ( age <=3 ){
 						BABY_PRICE;
 						}	else if ( age >3 && age <= 12){
@@ -127,7 +95,7 @@ while(1){
 						prise = ALL_DAY_CHILD_PRICE;
 						}
 						break;
-					
+
 					case 2:
 					if ( age <=3 ){
 						BABY_PRICE;
@@ -176,10 +144,11 @@ while(1){
 				default:
 					printf("잘 못 입력하셨습니다. ");
 		}
-	
+	}
+	void discountCase(){//우대사항 별 할인 			
 		switch(discount){
 			case 1:
-				final = prise;
+				final = prise; 
 				break;
 			case 2:
 				final = prise*0.5;
@@ -209,7 +178,50 @@ while(1){
 				} //파크는 할인적용 제외 
 				break;					
 		}
-			sum = final * orderCount;
+	}
+
+
+int main(){
+	
+
+while(1){
+	int totalTicketPrice=0, counter=0;
+	int icketTimeCaseArr[10]={0}, ageArr[10]={0}, ticketPriceArr[10]={0}, discountArr[10]={0}, orderCountArr[10]={0};
+	do{//c변수 초기화
+	 	selectTicket();
+		personNumber();
+		countTicket();
+		discountMoney();
+		monthAge();
+	
+			//나이 케이스 정립
+			if ( age <KID_BABY_AGE && age >=BABY_AGE_MIN) { 
+				ageCase=4; // 베이비 
+			} else if ((age >= KID_BABY_AGE && age <TEEN_KID_AGE)) {
+				ageCase = 3; // 어린이 
+			} else if (age >= TEEN_KID_AGE && age <= TEEN_AGE_MAX) {
+				ageCase = 2; //청소년 
+			} else if ( age >= OLDMAN_AGE ) {
+				ageCase = 5; //노 인  
+			} else {
+				ageCase = 1; // 어른 
+			}
+	
+			
+			//종합/파크&& 주간야간 선택 
+			if (choose==1 && ticket==1) {
+				ticketTimeCase = 1; //종합 + 1Day 
+			} else if (choose==1 && ticket==2) { 
+				ticketTimeCase =2; //종합 + 4시이후  
+				ticketTimeCase = 3; //파크 + 1Day 
+			} else {
+				ticketTimeCase = 4; // 파크 + 4시후 
+			} 
+
+		chooseAgePrice();
+		discountCase();
+		//총 합 가격 
+		sum = final * orderCount;
 	
 		printf(" *****************************\n");
 		printf(" 가격은 %d원 입니다.\n 감사합니다.\n",sum);
